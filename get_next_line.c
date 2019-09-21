1ebb910e07e5cce7bbcc6c16499a025b59a0b797
M1ZoN <nazim.islamov99@gmail.com>
2019-09-20 17:21:17 -0700

    last update

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mislamov <mislamov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/05 15:00:24 by mislamov          #+#    #+#             */
-/*   Updated: 2019/09/20 17:06:00 by mislamov         ###   ########.fr       */
+/*   Updated: 2019/09/20 17:17:46 by mislamov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,10 @@ int		helper(char **str, char **line, const int fd)
 	{
 		*line = ft_strsub(str[fd], 0, index);
 		tmp = ft_strdup(str[fd] + index + 1);
-		free(str[fd]);
+		ft_strdel(&str[fd]);
 		str[fd] = tmp;
 		if (str[fd][0] == '\0')
-			free(str[fd]);
+			ft_strdel(&str[fd]);
 	}
 	else if (str[fd][index] == '\0')
 	{
@@ -47,19 +47,19 @@ int		get_next_line(const int fd, char **line)
 	if (fd < 0 || fd > FD_SIZE || !line)
 		return (-1);
 	if (!str[fd])
-		str[fd] = ft_strnew(0);
+		str[fd] = ft_strnew(1);
 	while ((byte_size = read(fd, buffer, BUFF_SIZE)) > 0)
 	{
 		buffer[byte_size] = '\0';
 		temp = ft_strjoin(str[fd], buffer);
-		free(str[fd]);
+		ft_strdel(&str[fd]);
 		str[fd] = temp;
 		if (ft_strrchr(str[fd], '\n'))
 			break ;
 	}
 	if (byte_size < 0)
 		return (-1);
-	else if (str[fd][0] == '\0' && byte_size == 0)
+	else if (str[fd][0] == '\0' && !byte_size)
 		return (0);
 	return (helper(str, line, fd));
 }
